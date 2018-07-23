b6711371ad634d287a4a42bd868e6dc5b2652afe
Autumn Wilson <AutuOtter@Pearl.local>
2018-07-23 15:46:38 -0400

    S5 L65 - Refactoring Using KeyValuePairResource

--- Controllers/FeaturesController.cs
+++ Controllers/FeaturesController.cs
@@ -19,11 +19,11 @@ namespace vega.Controllers
             this.context = context;
         }
         [HttpGet("/api/features")]
-        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        public async Task<IEnumerable<KeyValuePairResource>> GetFeatures()
         {
             var features = await context.Features.ToListAsync();
 
-            return mapper.Map<List<Feature>, List<FeatureResource>>(features);
+            return mapper.Map<List<Feature>, List<KeyValuePairResource>>(features);
         }
     }
 }
--- Controllers/Resources/VehicleResource.cs
+++ Controllers/Resources/VehicleResource.cs
@@ -7,18 +7,18 @@ namespace vega.Controllers.Resources
     public class VehicleResource
     {
         public int Id { get; set; }
-        public ModelResource Model { get; set; }
-        public MakeResource Make { get; set; }
+        public KeyValuePairResource Model { get; set; }
+        public KeyValuePairResource Make { get; set; }
         public bool IsRegistered { get; set; }
         //add features here later
         public ContactResource Contact { get; set; }
         public DateTime LastUpdate { get; set; }
         // Many-to-Many relationship w/ features
-        public ICollection<FeatureResource> Features { get; set; }
+        public ICollection<KeyValuePairResource> Features { get; set; }
 
         public VehicleResource()
         {
-            Features = new Collection<FeatureResource>();
+            Features = new Collection<KeyValuePairResource>();
         }
     }
 }
