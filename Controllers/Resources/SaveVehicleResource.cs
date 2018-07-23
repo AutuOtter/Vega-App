901b1a7d6efbf2ff983353c69db192a6c6d84cb6
Autumn Wilson <AutuOtter@Pearl.local>
2018-07-23 15:24:58 -0400

    S5 L64 - Refactoring API Resources

--- Controllers/Resources/SaveVehicleResource.cs
+++ Controllers/Resources/SaveVehicleResource.cs
@@ -1,24 +1,25 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using vega.Models;
 
 namespace vega.Controllers.Resources
 {
-    public class VehicleResource
+    public class SaveVehicleResource
     {
         public int Id { get; set; }
-        public ModelResource Model { get; set; }
-        public MakeResource Make { get; set; }
+        public int ModelId { get; set; }
         public bool IsRegistered { get; set; }
         //add features here later
+        [Required]
         public ContactResource Contact { get; set; }
-        public DateTime LastUpdate { get; set; }
         // Many-to-Many relationship w/ features
-        public ICollection<FeatureResource> Features { get; set; }
+        public ICollection<int> Features { get; set; }
 
-        public VehicleResource()
+        public SaveVehicleResource()
         {
-            Features = new Collection<FeatureResource>();
+            Features = new Collection<int>();
         }
     }
 }
