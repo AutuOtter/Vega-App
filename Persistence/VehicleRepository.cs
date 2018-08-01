1981878d8d862a0bf71899ee91022be4c08ecd52
Autumn Wilson <AutuOtter@Pearl.local>
2018-08-01 10:45:59 -0400

    S7 L105 - Refactoring using Dictionaries

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using vega.Core;
@@ -39,24 +41,26 @@ namespace vega.Persistence
                 .Include(v => v.Features).ThenInclude(VehicleFeature => VehicleFeature.Feature)
                 .AsQueryable();
         
+            // Filtering
             if (queryObj.MakeId.HasValue)
                 query = query.Where(v => v.Model.MakeId == queryObj.MakeId.Value);
 
             if (queryObj.ModelId.HasValue)
                 query = query.Where(v => v.ModelId == queryObj.ModelId.Value);
             
+            // Sorting
+            var columnsMap = new Dictionary<string, Expression<Func<Vehicle, object>>>()
+            {
+                ["make"] = v => v.Model.Make.Name,
+                ["model"] = v => v.Model.Name,
+                ["contactName"] = v => v.ContactName,
+                ["id"] = v => v.Id
+            };
 
-            if (queryObj.SortBy == "make")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Make.Name) : query.OrderByDescending(v => v.Model.Make.Name);
-            
-            if (queryObj.SortBy == "model")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Name) : query.OrderByDescending(v => v.Model.Name);
-            
-            if (queryObj.SortBy == "contactName")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.ContactName) : query.OrderByDescending(v => v.ContactName);
-
-            if (queryObj.SortBy == "id")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Id) : query.OrderByDescending(v => v.Id);
+            if (queryObj.IsSortAscending)
+                query = query.OrderBy(columnsMap[queryObj.SortBy]);
+            else
+                query = query.OrderByDescending(columnsMap[queryObj.SortBy]);
 
             return await query.ToListAsync();
         }
